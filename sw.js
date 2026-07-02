d35c5fd2b11090cbfa68ddb63a6b55948a5f3f28
stevo227 <stevo227@protonmail.com>
2026-07-02 08:14:44 +0200

    Avatar Quiz - plne funkcny s 1812 otazkami, PWA, opravene geometria a audio

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'avatar-kviz-v1';
+const URLS = [
+  'Avatar_Kviz_Deti_v2.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => r))
+  );
+});
